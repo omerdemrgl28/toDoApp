da458c5ed647245567af549bec2e212d797a8a01
omerdemrgl28 <tanjantcotanjant372@gmail.com>
2024-01-26 20:04:05 +0300

    toDoApp
    toDoApp

---
+++ TodoApp/src/App.js
@@ -0,0 +1,49 @@
+import Task from './Task';
+import { useState } from 'react';
+import './App.css';
+import CreateTask from './CreateTask';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const addTask = (newTask) => {
+    debugger;
+    setTasks((prevTask) => {
+      return [...prevTask, newTask];
+    });
+  };
+
+  const deleteTask = (id) => {
+    debugger;
+    setTasks((prevTask) => {
+      return prevTask.filter((taskItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+  return (
+    <div className="App">
+      <Header />
+      <CreateTask onAdd={addTask} />
+      <div className="container">
+        <div className="row my-5">
+          {tasks.map((taskItem, index) => {
+            return (
+              <Task
+                key={index}
+                id={index}
+                title={taskItem.title}
+                content={taskItem.content}
+                delete={deleteTask}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
